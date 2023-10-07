33a1767103374e323b67f944546f0fcdc2fbfa2e
Daulet <daulet@qit.kz>
2023-10-07 13:04:45 +0600

    add math problems

--- main.py
+++ main.py
@@ -17,6 +17,28 @@ CHAT_ID = -1001718975100
 
 bot = telebot.TeleBot(TOKEN)
 
+message_counter = 0
+correct_answer = None  
+
+
+# Создаем подключение к базе данных или создаем файл базы данных, если он не существует
+conn = sqlite3.connect('user_scores.db')
+cursor = conn.cursor()
+
+# Создаем таблицу для хранения информации о пользователях и их успехах
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS user_scores (
+    user_id INTEGER PRIMARY KEY,
+    username TEXT,
+    score INTEGER DEFAULT 0
+)
+''')
+
+# Сохраняем изменения в базе данных и закрываем соединение
+conn.commit()
+conn.close()
+
+
 
 # Функция для отправки "пинга" каждые 30 минут
 def send_ping():
@@ -74,6 +96,77 @@ def send_anonymous(message):
         bot.register_next_step_handler(message, save_anonymous_message)
     else:
         bot.send_message(message.chat.id, "Команда /send_anonymous работает только в личных сообщениях с ботом.")
+
+# Функция для генерации математической задачи
+def generate_math_problem():
+    # Генерируем два случайных числа в диапазоне от 1 до 100
+    num1 = random.randint(1, 100)
+    num2 = random.randint(1, 100)
+
+    # Выбираем случайную математическую операцию (сложение, вычитание, умножение)
+    operation = random.choice(['+', '-', '*'])
+
+    # Генерируем текст задачи
+    if operation == '+':
+        problem_text = f"{num1} + {num2} = ?"
+        answer = num1 + num2
+    elif operation == '-':
+        problem_text = f"{num1} - {num2} = ?"
+        answer = num1 - num2
+    elif operation == '*':
+        problem_text = f"{num1} * {num2} = ?"
+        answer = num1 * num2
+
+    return problem_text, answer
+
+# Обработчик входящих сообщений
+@bot.message_handler(func=lambda message: True)
+def handle_message(message):
+    global message_counter, correct_answer
+
+    # Увеличиваем счетчик сообщений после каждого сообщения
+    message_counter += 1
+
+    # Если достигнуто 20 сообщений, отправляем новую математическую задачу
+    if message_counter % 20 == 0:
+        problem, correct_answer = generate_math_problem()
+        bot.send_message(message.chat.id, problem)
+
+    # Если пользователь отправил ответ на арифметическую задачу
+    if message_counter % 20 != 0 and message.text.isdigit():
+        user_answer = int(message.text)
+
+        # Проверяем правильность ответа пользователя
+        if user_answer == correct_answer:
+            bot.send_message(message.chat.id, "Правильно! Вам начислено 5 баллов.")
+
+            # Обновляем счет пользователя в базе данных
+            update_user_score(message.from_user.id, 5)
+
+        else:
+            bot.send_message(message.chat.id, "Неправильно. Попробуйте еще раз.")
+
+# Функция для обновления счета пользователя в базе данных
+def update_user_score(user_id, points):
+    conn = sqlite3.connect('user_scores.db')
+    cursor = conn.cursor()
+
+    # Проверяем, есть ли пользователь в базе данных
+    cursor.execute('SELECT * FROM user_scores WHERE user_id = ?', (user_id,))
+    user_data = cursor.fetchone()
+
+    if user_data is None:
+        # Если пользователь не найден, добавляем его в базу данных
+        cursor.execute('INSERT INTO user_scores (user_id, username, score) VALUES (?, ?, ?)', (user_id, '', points))
+    else:
+        # Если пользователь найден, обновляем его счет
+        current_score = user_data[2]
+        new_score = current_score + points
+        cursor.execute('UPDATE user_scores SET score = ? WHERE user_id = ?', (new_score, user_id))
+
+    conn.commit()
+    conn.close()
+
 
 
 # Запуск бота
